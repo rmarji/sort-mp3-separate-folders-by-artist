7a68d08cead3c86123f8b8c878976268055132bd
Raed Marji <raed@jogeeks.com>
2013-01-26 02:22:23 +0200

    this is to teach firas about git

--- create_tagged_mp3.sh
+++ create_tagged_mp3.sh
@@ -12,9 +12,10 @@
 #-----------------------------------------------
 
 count="0"
+filename = "change_this_to_your_file_name"
 for i in {1..10} #you can change the number of files created by changing 10 to any number u want
 do
-   touch new$count.mp3 # creates the file in the format of new0.mp3, new1.mp3 etc,,,
+   touch $filename$count.mp3 # creates the file in the format of new0.mp3, new1.mp3 etc,,,
    id3tag -s songname$count new$count.mp3 # this tags the song name or title
    id3tag -a artistname$count new$count.mp3 # this tags the artist name 
    id3tag -A albumname$count new$count.mp3 # this tags the album name
